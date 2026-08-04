5bb35d2373cab2299282f6ddf3851d3d2c0f5ccf
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-04 11:17:57 +0500

    solve leetcode 3731: find missing elements

---
+++ August challange/find_missing_elements.py
@@ -0,0 +1,12 @@
+class Solution:
+  def findMissingElements(self, nums: list[int]) -> list[int]:
+    min_val = min(nums)
+    max_val = max(nums)
+    nums_set = set(nums)
+    
+    missing = []
+    for num in range(min_val, max_val + 1):
+      if num not in nums_set:
+        missing.append(num)
+        
+    return missing
