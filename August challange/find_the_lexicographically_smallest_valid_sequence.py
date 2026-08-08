ccad8fe7352a37ed713bd31f5f6e2d96e09f8d6c
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-08 19:34:49 +0500

    solve leetcode 3302: find the lexicographically smalles valid sequence

---
+++ August challange/find_the_lexicographically_smallest_valid_sequence.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+  def validSequence(self, word1: str, word2: int) -> List[int]:
+    last = [-1] * len(word2)
+    i = len(word1) - 1
+    j = len(word2) - 1
+
+    while i >= 0 and j >= 0:
+      if word1[i] == word2[j]:
+        last[j] = i
+        j -= 1
+      i -= 1
+
+    ans = []
+    can_skip = True
+    j = 0
+
+    for i in range(len(word1)):
+      if j == len(word2):
+        break
+
+      if word1[i] == word2[j]:
+        ans.append(i)
+        j += 1
+      elif can_skip and (j == len(word2) - 1 or i < last[j + 1]):
+        can_skip = False
+        ans.append(i)
+        j += 1
+
+    return ans if j == len(word2) else []
