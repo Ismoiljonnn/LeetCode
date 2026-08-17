786dc02c608bac6c829c37e3c82ea67a47abf013
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-17 23:52:47 +0500

    solve 1563: Stone Game V

--- August challange/stone_game_V.py
+++ August challange/stone_game_V.py
@@ -1,36 +1,36 @@
-﻿from functools import cache
-
-class Solution:
+﻿class Solution:
     def stoneGameV(self, stoneValue: list[int]) -> int:
         n = len(stoneValue)
         pref = [0] * (n + 1)
         for i in range(n):
             pref[i + 1] = pref[i] + stoneValue[i]
-            
-        def get_sum(i, j):
-            return pref[j + 1] - pref[i]
 
-        @cache
-        def dp(i, j):
-            if i == j:
-                return 0
-            
-            max_score = 0
-            for k in range(i, j):
-                left_sum = get_sum(i, k)
-                right_sum = get_sum(k + 1, j)
-                
-                if left_sum < right_sum:
-                    max_score = max(max_score, left_sum + dp(i, k))
-                elif left_sum > right_sum:
-                    max_score = max(max_score, right_sum + dp(k + 1, j))
+        dp = [[0] * n for _ in range(n)]
+        max_l = [[0] * n for _ in range(n)]
+        max_r = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            max_l[i][i] = stoneValue[i]
+            max_r[i][i] = stoneValue[i]
+
+        for l in range(2, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                total = pref[j + 1] - pref[i]
+
+                k = i
+                while (pref[k + 1] - pref[i]) * 2 < total:
+                    k += 1
+
+                if (pref[k + 1] - pref[i]) * 2 == total:
+                    val = max(max_l[i][k], max_r[k + 1][j])
                 else:
-                    max_score = max(
-                        max_score, 
-                        left_sum + dp(i, k), 
-                        right_sum + dp(k + 1, j)
-                    )
-                    
-            return max_score
+                    left_val = max_l[i][k - 1] if k > i else 0
+                    right_val = max_r[k + 1][j] if k < j else 0
+                    val = max(left_val, right_val)
 
-        return dp(0, n - 1)
+                dp[i][j] = val
+                max_l[i][j] = max(max_l[i][j - 1], val + total)
+                max_r[i][j] = max(max_r[i + 1][j], val + total)
+
+        return dp[0][n - 1]
