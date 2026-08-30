19e3a8d1947b73220936d821a07e712b5db4be94
Ismoiljon <joraboyevismoiljon649@gmail.com>
2026-08-30 23:58:16 +0500

    solve leetcode 242: valid anagram

---
+++ valid_anagram.py
@@ -0,0 +1,17 @@
+# leetcode 242: valid anagram
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+
+    seen = {}
+
+    for char in s:
+      seen[char] = seen.get(char, 0) + 1
+
+    for char in t:
+      if char not in seen or seen[char] == 0:
+        return False
+      seen[char] - 1
+
+    return True
